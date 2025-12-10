34f2fd3d60668246e22da6a145cfa743df706441
Mischa Diehm <mischa.diehm@narrowin.ch>
2025-12-10 23:57:59 +0100

    get rid off attic - finally

--- attic/src-old/network_toolkit/platforms/cisco_ios/__init__.py
+++
@@ -1,6 +0,0 @@
-# SPDX-License-Identifier: MIT
-"""Cisco IOS platform implementation."""
-
-from network_toolkit.platforms.cisco_ios.operations import CiscoIOSOperations
-
-__all__ = ["CiscoIOSOperations"]
--- attic/src-old/network_toolkit/platforms/mikrotik_routeros/__init__.py
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: MIT
-"""MikroTik RouterOS platform implementation."""
-
-from network_toolkit.platforms.mikrotik_routeros.operations import (
-    MikroTikRouterOSOperations,
-)
-
-__all__ = ["MikroTikRouterOSOperations"]
